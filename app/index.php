50af23c33cbbb41758a839ed13595aafd33412ed
wdjacobo <113604892+wdjacobo@users.noreply.github.com>
2026-08-07 08:05:47 +0200

    Update defatult app content and styles

--- app/index.php
+++ app/index.php
@@ -1,16 +1,24 @@
 <?php
 
+$h1 = "Placeholder app";
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>App</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1><?= "Placeholder app" ?></h1>
+    <div>
+        <h1><?= $h1 ?></h1>
+        <p>Replace app folder contents and reload this page to see the changes</p>
+    </div>
 </body>
+
 </html>
